4db79236bb2fc9d91dab973b41b3d11a05fb4c6e
Alex Beeken <aeb242@gmail.com>
2015-02-27 14:40:12 -0800

    Display authors and beats correctly

--- controllers/AuthorsController.js
+++ controllers/AuthorsController.js
@@ -1,7 +1,7 @@
 newspaper.controller('AuthorsCtrl', function AuthorsCtrl($scope, $stateParams, BeatsFactory, UtilitiesFactory) {
   $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.beatId);
   $scope.addAuthor = function() {
-    $scope.beat.authors.push({ name: $scope.name, yearsEmployed: $scope.yearsEmployed, birthday: $scope.birthday});
+    $scope.beat.authors.push({ name: $scope.name, yearsEmployed: $scope.yearsEmployed, birthday: $scope.birthday });
     $scope.name = null;
     $scope.yearsEmployed = null;
     $scope.birthday = null;
